9781b079694152476b051de40141aee9bad3fe64
Anth <129230009+onlyanth@users.noreply.github.com>
2023-04-28 19:07:57 -0500

    Create db.hpp

---
+++ Security/db.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace databin {
+    static const uint8_t data[] = {
+        0xDE, 0xAD, 0xBE, 0xEF
+    };
+    static const size_t size = sizeof(data);
+}
