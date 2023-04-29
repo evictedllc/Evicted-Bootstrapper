19102e2c8abaeb25be802fc254b8dac43575430d
Anth <129230009+onlyanth@users.noreply.github.com>
2023-04-28 19:03:10 -0500

    Update Anti-Debug.hpp

---
+++ Security/Anti-Debug.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../Misc/Static.hpp"
+#include "./AntiTamper.hpp"
+#include "SysHook.hpp"
+#include "../Security/FunctionReader.hpp"
+
+#pragma warning(disable : 4191)
+namespace syn::AntiDebug
+{
+__forceinline bool Setup()
+{
+// Attempt to prevent debugging
+bool success = AntiTamper::Initialize();
+success &= SysHook::DisableDebuggerDetection();
+success &= FunctionReader::HideModuleFromPEB();
+
+	// If the above fails, run default behavior
+	if (!success)
+	{
+		// TODO: implement default behavior
+		return false;
+	}
+
+	return true;
+}
+
+__forceinline bool Check()
+{
+	// Perform checks for debugging
+	bool isDebugging = false;
+
+	// TODO: implement checks
+
+	return isDebugging;
+}
